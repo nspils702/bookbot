19d79601a2ffd2b11cf274688ebb107a2b8513fa
Neal Spilsbury <nspils702@gmail.com>
2023-02-19 19:47:00 -0800

    created a function that can count character instances in a book

---
+++ main.py
@@ -0,0 +1,18 @@
+with open("books/frankenstein") as f:
+    file_contents = f.read()
+def counter():
+    char_count = dict()
+    strings = file_contents.split()
+    for string in strings:
+        for char in string:
+            if char.isalpha(): 
+                char = char.lower()
+                if char in char_count:
+                    char_count[char] += 1
+                else:
+                    char_count[char] = 1
+    myKeys = list(char_count.keys())
+    myKeys.sort()
+    sorted_dict = {i: char_count[i] for i in myKeys}
+    print(sorted_dict)
+counter()
